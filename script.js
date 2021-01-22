cf4461586417d37c3ee988a9ac091f0b2a6eec13
Andrii Davnich <d.a.lonerwolf@gmail.com>
2021-01-22 23:03:37 +0200

    full-on-one-display

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 $('.carousel').carousel({
     interval: false
 })
+
+
+
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:200,
+  dots: false,
+})
+
+
+var owl = $('.owl-carousel');
+owl.owlCarousel();
+// Go to the next item
+$('.right').click(function() {
+  owl.trigger('next.owl.carousel');
+})
+// Go to the previous item
+$('.left').click(function() {
+  // With optional speed parameter
+  // Parameters has to be in square bracket '[]'
+  owl.trigger('prev.owl.carousel', [300]);
+})
